3b28bea5be8ac10e0a29168db31f94b4871beb47
Gabriel Lindström <gabriel.lindstrom@gmail.com>
2019-11-14 21:40:57 +0200

    Exercise 3.4 done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 let persons = [
   {
@@ -22,28 +22,40 @@ let persons = [
     number: "39-23-6423122",
     id: 4
   }
+];
 
-]
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(p => p.id === id)
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(p => p.id === id);
 
   if (person) {
-    response.json(person)
+    response.json(person);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
-})
+});
 
-app.get('/info', (req, res) => {
-  res.send('<div>Phonebook has info for ' + persons.length + ' people</div><div>'+ new Date() +'</div>')
-})
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(p => p.id !== id);
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+  res.status(204).end();
+});
+
+app.get("/info", (req, res) => {
+  res.send(
+    "<div>Phonebook has info for " +
+      persons.length +
+      " people</div><div>" +
+      new Date() +
+      "</div>"
+  );
+});
+
+const port = 3001;
+app.listen(port);
+console.log(`Server running on port ${port}`);
