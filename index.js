f2f123e8bf66f8d08ed56a9ea6d400914b282bb0
Gabriel Lindström <gabriel.lindstrom@gmail.com>
2019-11-14 21:29:20 +0200

    Exercise 3.3 done

--- index.js
+++ index.js
@@ -10,23 +10,34 @@ let persons = [
   {
     name: "Ada LoveLace",
     number: "39-44-5323523",
-    id: 4
+    id: 2
   },
   {
     name: "Dan Abramov",
     number: "12-43-234345",
-    id: 4
+    id: 3
   },
   {
     name: "Mary Poppendieck",
     number: "39-23-6423122",
-    id: 3
+    id: 4
   }
 
 ]
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
 
 app.get('/info', (req, res) => {
