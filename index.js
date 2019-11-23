dd7e884a429b74b945fa4e58514dac840d77cb9d
Gabriel Lindström <gabriel.lindstrom@gmail.com>
2019-11-23 11:13:43 +0200

    Exercise 3.15 done

--- index.js
+++ index.js
@@ -82,10 +82,11 @@ app.post('/api/persons', (request, response) => {
 });
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter(p => p.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end;
+    })
+    .catch(error => next(error));
 });
 
 app.get('/info', (request, response) => {
