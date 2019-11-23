60e442dbcf744aa582ca7ed4d886cc5cefe149da
Gabriel Lindström <gabriel.lindstrom@gmail.com>
2019-11-23 13:22:27 +0200

    uibuild

--- models/person.js
+++ models/person.js
@@ -13,8 +13,8 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: {type: String, unique: true},
-  number: String
+  name: {type: String, minlength: 3, unique: true},
+  number: {type: String, minlength: 8}
 });
 personSchema.plugin(uniqueValidator);
 
