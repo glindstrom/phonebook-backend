a810d2e238f168a5ca4c395a85dfcbeedfda1820
Gabriel Lindström <gabriel.lindstrom@gmail.com>
2019-11-23 10:30:54 +0200

    Exercise 3.14 done

--- index.js
+++ index.js
@@ -1,72 +1,54 @@
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const Person = require('./models/person');
 
-morgan.token("req-body", (req, res) => {
+morgan.token('req-body', (req, res) => {
   return JSON.stringify(req.body);
 });
 
-app.use(express.static("build"));
+app.use(express.static('build'));
 app.use(bodyParser.json());
 app.use(
   morgan(
-    ":method :url :status :res[content-length] - :response-time ms :req-body"
+    ':method :url :status :res[content-length] - :response-time ms :req-body'
   )
 );
 app.use(cors());
 
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
-const url = `mongodb+srv://fullstack:${MONGO_PASSWORD}@cluster0-ikdln.mongodb.net/phonebook?retryWrites=true&w=majority`;
-mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
-});
-
-personSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  }
-});
-
-const Person = mongoose.model("Person", personSchema);
-
 let persons = [
   {
-    name: "Arto Hellas",
-    number: "040-123456",
+    name: 'Arto Hellas',
+    number: '040-123456',
     id: 1
   },
   {
-    name: "Ada LoveLace",
-    number: "39-44-5323523",
+    name: 'Ada LoveLace',
+    number: '39-44-5323523',
     id: 2
   },
   {
-    name: "Dan Abramov",
-    number: "12-43-234345",
+    name: 'Dan Abramov',
+    number: '12-43-234345',
     id: 3
   },
   {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
     id: 4
   }
 ];
 
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons.map(person => person.toJSON()));
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id);
   const person = persons.find(p => p.id === id);
 
@@ -77,48 +59,46 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
-app.post("/api/persons", (request, response) => {
-  const person = request.body;
-  if (!person.name) {
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+  if (!body.name) {
     return response.status(400).json({
-      error: "name is missing"
+      error: 'name is missing'
     });
-  } else if (!person.number) {
+  } else if (!body.number) {
     return response.status(400).json({
-      error: "number is missing"
-    });
-  }
-
-  if (persons.some(p => p.name === person.name)) {
-    return response.status(400).json({
-      error: "name must be unique"
+      error: 'number is missing'
     });
   }
 
-  const id = Math.floor(Math.random() * Math.floor(Number.MAX_SAFE_INTEGER));
-  person.id = id;
-  persons = persons.concat(person);
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  });
 
-  response.json(person);
+  person.save().then(savedPerson => {
+    response.json(savedPerson.toJSON());
+  });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id);
   persons = persons.filter(p => p.id !== id);
 
   response.status(204).end();
 });
 
-app.get("/info", (request, response) => {
+app.get('/info', (request, response) => {
   response.send(
-    "<div>Phonebook has info for " +
+    '<div>Phonebook has info for ' +
       persons.length +
-      " people</div><div>" +
+      ' people</div><div>' +
       new Date() +
-      "</div>"
+      '</div>'
   );
 });
 
-const port = process.env.PORT || 3001;
-app.listen(port);
-console.log(`Server running on port ${port}`);
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
